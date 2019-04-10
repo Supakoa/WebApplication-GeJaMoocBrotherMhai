75f8e42ec0088e156e86095531c9f5c6221896b9
supakoa <41123305+Supakoa@users.noreply.github.com>
2019-04-11 02:44:44 +0700

    koa

---
+++ GEMooc/font-end/pages/course/subpage/index.php
@@ -0,0 +1,30 @@
+<div class="container cez">
+
+</div>
+<div class="container" style="z-index:3">
+    <div class="row">
+        <div class="col-sm-4 offset-sm-4 text-center">
+        <button type="button" class="btn btn-info">Go to course</button>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-5 offset-lg-2 table-responsive mt-4">
+            <table class="table table-striped">
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>6546</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>6546</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>6546</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
---
+++ GEMooc/font-end/pages/howto/index.php
@@ -0,0 +1,15 @@
+<div class=" cecontent mt-2">
+    <div class="card-body shadow "></div>
+    <div class="container-fluid" style="background:black">
+        <div class="container single-item text-center">
+            <div><img class="img-fluid shadow" src="image/abc.png" width="100%" height="100%" alt=""></div>
+            <div>your content</div>
+            <div>your content</div>
+        </div>
+    </div>
+</div>
+<script>
+    $(document).ready(function() {
+        $('.single-item').slick();
+    });
+</script>
