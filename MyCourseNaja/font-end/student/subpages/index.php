cb3628186c93978c12d8e0f67f443f9fa49c54ad
supakoa <41123305+Supakoa@users.noreply.github.com>
2019-04-14 19:07:03 +0700

    koa

--- MyCourseNaja/font-end/student/subpages/index.php
+++ MyCourseNaja/font-end/student/subpages/index.php
@@ -1,51 +1,59 @@
 <div class="cez">
     <h5 class="card-title">My course</h5>
-    <div class="row">
-        <div class="col-lg-4">
-            <a class="btn" href="#">
-                <div class="card text-white bg-dark " style="max-width: 18rem;">
-                    <div class="card-header">Header</div>
-                    <div class="card-body">
-                        <h5 class="card-title">Dark card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div>
-            </a>
+    <div class="row ">
+        <div class="col-lg cardce ">
+            <a href="#" data-toggle="modal" data-target=".createcourse"> <img src="image/fjords.jpg" class="img-fluid rounded mx-auto d-block mb-5 shadow" alt="..."></a>
         </div>
-        <div class="col-lg-4 text-center">
-            <a class="btn mt-5" href="#"><i class="fas fa-plus-circle " ></i></a>
-        </div>
+
     </div>
     <hr class="mt-3">
     <nav class="navbar navbar-light bg-light">
-        <!-- <a class="navbar-brand">Navbar</a> -->
+        <a class="navbar-brand">Total Course 3</a>
         <form class="form-inline">
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
     </nav>
-    <div class="row">
-        <div class="col-lg-4">
-            <a class="btn" href="#">
-                <div class="card text-white bg-dark " style="max-width: 18rem;">
-                    <div class="card-header">Header</div>
-                    <div class="card-body">
-                        <h5 class="card-title">Dark card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div>
-            </a>
+
+    <div class="row wow bounceInRight mt-3">
+        <div class="col-lg cardce ">
+            <a href="#"> <img src="image/fjords.jpg" class="img-fluid rounded mx-auto d-block mb-5 shadow" alt="..."></a>
         </div>
-        <div class="col-lg-4 text-center">
-        <a class="btn" href="#">
-                <div class="card text-white bg-dark " style="max-width: 18rem;">
-                    <div class="card-header">Header</div>
-                    <div class="card-body">
-                        <h5 class="card-title">Dark card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div>
-            </a>
+        <div class="col-lg cardce">
+            <a href="#"> <img src="image/fjords.jpg" class="img-fluid rounded mx-auto d-block mb-5 shadow" alt="..."></a>
+        </div>
+        <div class="col-lg cardce">
+            <a href="#"> <img src="image/fjords.jpg" class="img-fluid rounded mx-auto d-block mb-5 shadow" alt="..."></a>
         </div>
     </div>
 </div>
+
+<!-- Large modal -->
+<!-- <button type="button" class="btn btn-primary" >Large modal</button> -->
+
+<div class="modal fade createcourse" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">subject name&& detail</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-info btn-md">Go to Mycourse</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<script>
+    $('.createcourse').modal({
+        keyboard: true;
+        backdrop: 'static'
+    })
+</script>
--- MyCourseNaja/font-end/student/subpages/profile.php
+++ MyCourseNaja/font-end/student/subpages/profile.php
@@ -1,1 +1,13 @@
-fasdfsdf
+<div class="row">
+    <dt class="col-sm-3">Full name :</dt>
+    <dd class="col-sm-9">Mr. Supakit kitjanabumrungsak</dd>
+
+    <dt class="col-sm-3">Total your course :</dt>
+    <dd class="col-sm-9"> 1 </dd>
+
+    <dt class="col-sm-3">facebook :</dt>
+    <dd class="col-sm-9"> Koa Supakit </dd>
+
+    <dt class="col-sm-3">Payment :</dt>
+    <dd class="col-sm-9"> Bangkok Bank </dd>
+</div>
