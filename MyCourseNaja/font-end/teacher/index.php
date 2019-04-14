52befe0f3d5374b8f60de2880ac0df2d1f160228
supakoa <41123305+Supakoa@users.noreply.github.com>
2019-04-14 19:10:04 +0700

    koa

--- MyCourseNaja/font-end/teacher/index.php
+++ MyCourseNaja/font-end/teacher/index.php
@@ -1,14 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- iconWeb -->
+    <!-- <link rel="icon" type="image/ico" href="image/GElogo.png" /> -->
     <title>My-course</title>
     <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="../node_modules/wow.js/css/libs/animate.css">
     <link rel="stylesheet" href="../node_modules/sweetalert2/dist/sweetalert2.min.css">
+
+    <!-- CEFstyle -->
+    <link rel="stylesheet" href="../node_modules/CEFstyle/CEFstyle.css">
+    <!-- fontawesom -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
 </head>
+
 <body>
 
 
@@ -21,10 +31,23 @@
 
 
 
-    <script src="../node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="../node_modules/jquery/dist/jquery.js"></script>
     <script src="../node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="../node_modules/popper.js/dist/popper.min.js"></script>
     <script src="../node_modules/sweetalert2/dist/sweetalert2.min.js"></script>
     <script src="../node_modules/wow.js/dist/wow.min.js"></script>
+    <!-- CEFstyle -->
+    <script src="../node_modules/CEFstyle/CEFstyle.js"></script>
+    <script>
+        wow = new WOW({
+            boxClass: 'wow', // default
+            animateClass: 'animated', // default
+            offset: 0, // default
+            mobile: true, // default
+            live: true // default
+        })
+        wow.init();
+    </script>
 </body>
+
 </html>
